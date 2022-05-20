98f391f042f38eb505eb238cf68d8fab5738a142
Aida Abdraimova <aida.abdyraimova.kk@gmail.com>
2022-05-20 14:51:06 +0600

    Add index.html, css, js files

---
+++ js/script1.js
@@ -0,0 +1,5 @@
+// 1. Дана строка 'ddd@bbb@ccc'. Замените все @ на '!'
+
+let srting = 'ddd@bbb@ccc';
+console.log(srting.replaceAll('@', '!'));
+document.write("1. Дана строка 'ddd@bbb@ccc'. Замените все @ на '!' - " + "  " + srting.replaceAll('@', '!'));
